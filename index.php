833d2eca84abf60cee401ea30fc7b6716e7adf3c
Айдос Тажиниязов <a.tazhiniazov@gmail.com>
2023-09-25 17:29:59 +0500

    initial commit

---
+++ index.php
@@ -0,0 +1,29 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>PHP-QrCode</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"/>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.0/jquery.min.js">
+    </script>
+</head>
+<body>
+<div class="container">
+    <div class="table-responsive">
+        <h3>QR Generation Form</h3><br/>
+        <div class="box">
+            <form method="post" action="#">
+                <div class="form-group" style="width: 40%">
+                    <label>QR Text</label>
+                    <input type="text" name="qrtext" id="qrtext" placeholder="Enter QR Text" required
+                           data-parsley-pattern="^[a-zA-Z]+$" data-parsley-trigger="keyup" class="form-control"/>
+                </div>
+                <div class="form-group">
+                    <input type="submit" name="generate-btn" value="QR Generate" class="btn btn-success"/>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+</body>
+</html>
